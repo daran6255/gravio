263792093cc1a956fee164228daf43676c80df5d
daran6255 <darandharani6255@gmail.com>
2026-06-20 20:23:39 +0530

    Successfully deleted the API Performance and Infrastructure Health components from the dashboard

--- frontend/src/pages/dashboard/Dashboard.tsx
+++ frontend/src/pages/dashboard/Dashboard.tsx
@@ -1,23 +1,11 @@
 import React from 'react';
 import { Box } from '@mui/material';
-import Grid from '@mui/material/Grid';
-import ApiSpeedometer from '../../components/dashboard/ApiSpeedometer';
-import SystemHealthMonitor from '../../components/dashboard/SystemHealthMonitor';
 import WelcomeHeader from '../../components/dashboard/WelcomeHeader';
 
 const Dashboard: React.FC = () => {
 	return (
 		<Box component="main" sx={{ p: { xs: 1, sm: 0 } }}>
 			<WelcomeHeader />
-
-			<Grid container spacing={3} sx={{ mt: 1, alignItems: 'stretch' }}>
-				<Grid size={{ xs: 12, md: 6 }}>
-					<ApiSpeedometer />
-				</Grid>
-				<Grid size={{ xs: 12, md: 6 }}>
-					<SystemHealthMonitor />
-				</Grid>
-			</Grid>
 		</Box>
 	);
 };
