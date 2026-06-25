f68aa6b0839bdfc813dd88f829316b372d053e79
daran6255 <darandharani6255@gmail.com>
2026-06-25 21:22:53 +0530

    deleted unwanted files in the data folder

--- frontend/src/data/Training.ts
+++
@@ -1,3 +0,0 @@
-export const TRAINING_MODES = ['Offline', 'Online', 'Hybrid'];
-export const DOMAINS = ['BFSI', 'IT', 'Accessibility Testing'];
-export const BATCH_TAGS = ['CSR', 'Internal', 'Govt', 'Special'];
